c6c075ffde0a3dce1ddb17620cef6ee540813a93
Ryan Morris <mr.morris@gmail.com>
2021-11-07 13:28:43 -0500

    Calculator implementation of get, mul, add, etc...

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -10,4 +10,16 @@ describe("Constructor Functions Exercise", function () {
     expect(typeof Calculator.prototype.mul).toBe("function");
     expect(typeof Calculator.prototype.get).toBe("function");
   });
+
+  it("Should add and multiply numbers", function () {
+    let c = new Calculator(10);
+
+    c.push(5);
+    c.add();
+    expect(c.get()).toBe(15);
+
+    c.push(2);
+    c.mul();
+    expect(c.get()).toBe(30);
+  });
 });
