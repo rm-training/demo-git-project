83c2c88843f3704fb2410df3d0d458ea6c2c69e1
Ryan Morris <mr.morris@gmail.com>
2021-11-07 13:29:16 -0500

    Calculator tests

--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -11,6 +11,14 @@ describe("Constructor Functions Exercise", function () {
     expect(typeof Calculator.prototype.get).toBe("function");
   });
 
+  it("Should default the initial value to zero", function () {
+    let c1 = new Calculator(10);
+    expect(c1.get()).toBe(10);
+
+    let c2 = new Calculator();
+    expect(c2.get()).toBe(0);
+  });
+
   it("Should add and multiply numbers", function () {
     let c = new Calculator(10);
 
@@ -21,5 +29,34 @@ describe("Constructor Functions Exercise", function () {
     c.push(2);
     c.mul();
     expect(c.get()).toBe(30);
+  });
+
+  it("Should add the numbers before it multiplies", function () {
+    let d = new Calculator(10);
+    d.push(2);
+    d.push(3);
+    d.mul();
+    expect(d.get()).toBe(50);
+  });
+
+  it("Should support unique objects", function () {
+    let c1 = new Calculator(10);
+    let c2 = new Calculator(5);
+
+    c1.push(2);
+    c2.push(1);
+    c1.add();
+    c2.add();
+
+    expect(c1.get()).toBe(12);
+    expect(c2.get()).toBe(6);
+  });
+
+  it("Should be able to run example code", function () {
+    let c = new Calculator(5);
+    c.push(5);
+    c.push(10);
+    c.add();
+    expect(c.get()).toBe(20);
   });
 });
